c55c7a954954ee9486d6f773742bc15c1f5c3b00
Syed Abbas <sabbas@MacBook-Pro-2.local>
2018-08-22 07:39:13 -0500

    Some additions

--- lib/spendthrift/cli.rb
+++ lib/spendthrift/cli.rb
@@ -25,10 +25,10 @@ case ARGV[0]
 
 when 'transactions'
 
-  start_date =  options.key?(:start_date) ? options[:start_date] : Date.today
-  end_date = options.key(:end_date)? options[:end_date]: Date.today
+  start_date = options.key?(:start_date) ? options[:start_date] : Date.today
+  end_date = options.key(:end_date) ? options[:end_date] : Date.today
   p = PlaidGateway::PlaidClient.new
-  t = p.get_all_transactions start_date:start_date, end_date:end_date
+  t = p.get_all_transactions start_date: start_date, end_date: end_date
 
   table = Hash.new {|hash, key| hash[key] = []}
 
@@ -39,15 +39,26 @@ when 'transactions'
 when 'test'
   contents = File.read('transactions.json')
   transactions = JSON.parse(contents)
+  transactions = transactions.sort_by {|hash| Date.parse hash['date']}
 
-  table = Hash.new {|hash, key| hash[key] =[]}
+  include_attributes = %w[amount category date name pending transaction_type]
+
+  table = Hash.new {|hash, key| hash[key] = []}
+
   transactions.each do |item|
-    item.each {|key, value| table[key].push(value)}
+    item.each {|key, value| table[key].push(value) if include_attributes.include? key}
   end
 
+  column_widths = {}
+  table.keys.each do |k|
+    column_widths[k] = table[k].max_by {|item| item.length if item.is_a? String}
+  end
 
+  table['category'] = table['category'].map {|category| category.join '-'}
 
-
+  table.keys.each do |k|
+    puts k, table[k].max_by {|item| item.length if item.is_a? String}.length unless k == 'amount'
+  end
 
 
 
--- spendthrift.gemspec
+++ spendthrift.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["sabbas"]
   spec.email         = ["syed.k.abbas1988@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = ''
+  spec.description   = ''
+  spec.homepage      = ""
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
